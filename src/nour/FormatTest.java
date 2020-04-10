f2118bb8dff970c85dda954ae6b4f7e5e72ab66c
Dr Noureddin Sadawi <n.sadawi@gmail.com>
2020-04-10 16:06:13 +0100

    first commit

---
+++ src/nour/FormatTest.java
@@ -0,0 +1,16 @@
+package nour;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FormatTest {
+
+ @Test
+ public void testFormat() {
+  Utility util = new Utility();
+  String result = util.format(13.5545);
+  assertEquals("13.55",result);
+ }
+
+}
